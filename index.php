d09861bce9d4dcf6abc34b6c0a1019cdf6b6eed8
kiuen123 <kien121999121999@gmail.com>
2021-04-20 01:45:50 +0700

    xoa phan thua

--- index.php
+++ index.php
@@ -1,3 +1,62 @@
 <?php include "./head.php" ?>
 
+<!-- slide show -->
+<div class="slide-show">
+        <div class="container">
+            <div class="row">
+                <!-- bắt đầu nội dung của slide show -->
+
+                <!-- slide trong này -->
+                <div class="col-md-9">
+                    <div class="slide">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="./resource/image/slide1.png" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="./resource/image/slide2.png" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="./resource/image/slide3.jpg" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="./resource/image/slide1.png" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="./resource/image/slide2.png" class="d-block w-100" alt="...">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev slide-button" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next slide-button" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                    <!-- về căn bản thì slide show thế này cũng dc nhưng tôi muốn chỉnh sửa nhiều hơn -->
+
+                    <!-- chưa biết thêm j vào đây -->
+                    <div class="col-md-3">
+                        <div class="???">
+
+                        </div>
+                    </div>
+
+                    <!-- kết thúc nội dung của slide show -->
+                </div>
+            </div>
+        </div>
+        <!-- end slide show -->
+
 <?php include "./tail.php" ?>
