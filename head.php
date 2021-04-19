3ec0c2df4ae6f64c5a8351ad24bbce725b3fe9de
kiuen123 <kien121999121999@gmail.com>
2021-04-20 01:35:25 +0700

    xoa phan thua

---
+++ head.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Class</title>
+    <meta property="og:image" content="#">
+    <meta name="description" content="miêu tả trang như muốn đấm vào mồm người xem">
+    <meta name="author" content="Nguyễn Trung Kiên">
+    <!-- css của tôi -->
+    <link rel="stylesheet" href="./css/home.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./resource/bootstrap-5/css/bootstrap.min.css">
+    <script src="../resource/bootstrap-5/js/bootstrap.bundle.min.js"></script>
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="./resource/fontawesome/css/all.css">
+</head>
+
+<body>
+<!-- nabbar -->
+<div class="top-nab">
+        <div class="container">
+            <div class="row">
+                <!-- bắt đầu nội dung của nabbar -->
+                <!-- logo, home, ... -->
+                <div class="col-md-9" style="display: flex;">
+                    <a href="./index.php">
+                        <!-- <img src="../resource/image/test.png" alt="" class="logo"> -->
+                        <img src="./resource/image/logo.png" alt="" class="logo">
+                    </a>
+                    <div style="font-size: 16px;
+                                vertical-align: top;
+                                color: #fff;
+                                position: relative;
+                                padding: 21px 20px;">
+                        | Online class
+                    </div>
+                    <a href="./index.php">
+                        <div class="nab-item">
+                            Trang chủ
+                        </div>
+                    </a>
+                    <a href="./class.php">
+                        <div class="nab-item">
+                            Lớp học
+                        </div>
+                    </a>
+                </div>
+                <!-- thanh tìm kiếm, đăng kí đăng nhập, ... -->
+                <div class="col-md-3" style="display: flex;">
+                    <div class="search-bar">
+                        <input type="text" class="search-bar-input">
+                        <button class="search-bar-button">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>
+                    <a href="./login-register.php" style="color: #fff !important;">
+                        <div class="nab-item" style="border-radius: 50%; 
+                                                width: 66px;">
+                            <i class="fas fa-user"></i>
+                        </div>
+                    </a>
+
+                </div>
+                <!-- kết thúc nội dung nabbar -->
+            </div>
+        </div>
+    </div>
+    <!-- end nabbar -->
+
+
