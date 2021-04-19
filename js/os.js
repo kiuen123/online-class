a12f2e7b40e6fad815152b02cf103b45f3942cb1
kiuen123 <kien121999121999@gmail.com>
2021-04-20 04:18:18 +0700

    met qua di ngu

--- js/os.js
+++ js/os.js
@@ -4,8 +4,8 @@ function roundToTwo(num) {
     return +(Math.round(num + "e+2") + "e-2");
 }
 
-console.log("type: " + os.type());
+console.log("Type: " + os.type());
 console.log("Platform: " + os.platform() + " " + os.arch() + "bit");
 console.log("Hostname: " + os.hostname());
-console.log("Number of CPUs: " + os.cpus().length);
-console.log("mem: " + roundToTwo(os.totalmem() / 1024 / 1024 / 1024) + "gb");
+console.log("Core CPU: " + os.cpus().length);
+console.log("Memories: " + roundToTwo(os.totalmem() / 1024 / 1024 / 1024) + "gb");
