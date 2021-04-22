2a5e7c265d488b805914aa13409873bba7bc3bf7
kiuen123 <kien121999121999@gmail.com>
2021-04-23 02:02:46 +0700

    test CSDL

--- js/os.js
+++ js/os.js
@@ -9,10 +9,3 @@ console.log("Nền tảng      : " + os.platform() + " " + os.arch() + " bit");
 console.log("Tên máy       : " + os.hostname());
 console.log("Số core CPU   : " + os.cpus().length);
 console.log("Dung lượng RAM: " + roundToTwo((os.totalmem() - os.freemem()) / 1024 / 1024 / 1024) + " / " + roundToTwo(os.totalmem() / 1024 / 1024 / 1024) + "gb");
-
-// var fs = require('fs');
-// var files = fs.readdirSync('./');
-// console.log(files);
-
-// npm install jquery
-// npm install express --save
