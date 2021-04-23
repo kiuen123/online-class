9e78daa9c0bb0f088247d3ee53ca2feb1f8c448f
kiuen123 <kien121999121999@gmail.com>
2021-04-23 09:16:48 +0700

    hay lam

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
                                 if (mysqli_num_rows($result) > 0) 
                                 {
                                     $row = mysqli_fetch_assoc($result);
-                                    if ($pass, $row['pass']) 
+                                    if ($pass= $row['pass']) 
                                     {
                                         header('Location: ./home.php');
                                     } 
