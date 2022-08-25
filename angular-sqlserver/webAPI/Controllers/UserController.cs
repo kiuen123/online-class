c07b04b5ac6812272dc970f0efb88cdd9365ce19
kiuen123 <kien121999121999@gmail.com>
2022-08-25 09:27:42 +0700

    25/08/2022 -2

--- angular-sqlserver/webAPI/Controllers/UserController.cs
+++ angular-sqlserver/webAPI/Controllers/UserController.cs
@@ -85,10 +85,33 @@ namespace webAPI.Controllers
             return new JsonResult(table);
         }
 
-        [HttpPatch("EditUserbyId")]
+        [HttpPatch("UpdateUserbyId")]
         public JsonResult EditUserbyId(EditUser user)
         {
             string query = "update users set email = '"+user.email+"', ten = N'"+user.ten+"', anh_dai_dien = '"+user.anh_dai_dien+"', tom_tat = N'"+user.tom_tat+"' where id = "+user.id+"";
+            WriteLog writeLog = new WriteLog();
+            writeLog.wirte(query);
+            DataTable table = new DataTable();
+            String sqlDataSource = _configuration.GetConnectionString("kteachlab");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand nyCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = nyCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpDelete("DeleteUserbyId")]
+        public JsonResult DeleteUserbyId(string id)
+        {
+            string query = "delete users where id = " + id + "";
             WriteLog writeLog = new WriteLog();
             writeLog.wirte(query);
             DataTable table = new DataTable();
